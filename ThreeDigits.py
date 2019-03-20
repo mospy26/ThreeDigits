73c91fcf69abe9ca81ac1e441192f60158d04815
Mustafa <m.mustafaahmed@hotmail.com>
2019-03-20 17:55:13 +1100

    First

---
+++ ThreeDigits.py
@@ -0,0 +1,26 @@
+import sys
+
+
+
+class State:
+    def __init__(self, state):
+        self.state = state
+    def __repr__(self):
+        return state
+
+    def add(direction):
+        return stte
+
+class Graph:
+    def __init__(self, vertices):
+        self.vertices = {key : value for (vertex, edges) in vertices.items()}
+    def BFS(start, end):
+        seen = []
+
+
+if __name__ == "__main__":
+    main()
+
+def main():
+    algorithm = sys.argv[1]
+    file = sys.argv[2]
